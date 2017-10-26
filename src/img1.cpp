8ffd852f58ef37c08b891c5300f8d0ee2d58a33c
jlrandulfe <jlrandulfe@gmail.com>
2017-10-26 18:37:46 +0200

    Reorganize the code flow, and add the whole pipeline to the script

--- src/img1.cpp
+++ src/img1.cpp
@@ -7,50 +7,40 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	// Read the first proposed image and get its histogram.
+    // Compression parameters
+    vector<int> comp;
+    comp.push_back(CV_IMWRITE_JPEG_QUALITY);
+    // 40 for compressed images, 100 por uncompressed.
+    comp.push_back(40);
+    // Read the first proposed image and get its histogram.
     cv::Mat image = cv::imread("./resources/Image1.png", cv::IMREAD_GRAYSCALE);
     cv::Mat hist_img = Tools::get_histogram(image);
+    cv::namedWindow("original image", cv::WINDOW_NORMAL);
+    cv::imshow("original image", image);
+    cv::imwrite("./results/image1/00-original_histrogram.png", hist_img);
     // Perform a max filter on the image and calculate its histogram.
-    cv::Mat filtered_img = Filters::max_filter(image, 5);
-    cv::Mat hist_filtered = Tools::get_histogram(filtered_img);
+    cv::Mat filtered_img_1 = Filters::max_filter(image, 1);
+    cv::Mat filtered_img_2 = Filters::max_filter(image, 2);
+    cv::Mat filtered_img_3 = Filters::max_filter(image, 3);
+    cv::imwrite("./results/image1/01-max_filter_k1.jpg", filtered_img_1, comp);
+    cv::imwrite("./results/image1/01-max_filter_k2.jpg", filtered_img_2, comp);
+    cv::imwrite("./results/image1/01-max_filter_k3.jpg", filtered_img_3, comp);
+    cv::namedWindow("filtered image", cv::WINDOW_NORMAL);
+    cv::imshow("filtered image", filtered_img_3);
     // Apply standard median filter.
     cv::Mat median_img; 
-    cv::medianBlur(filtered_img, median_img, 3);
-    cv::Mat hist_median = Tools::get_histogram(median_img);
-    // Enhance intensity values.
-    cv::Mat enhanced_img = Tools::log_transform(median_img);
-    cv::Mat hist_enhanced = Tools::get_histogram(enhanced_img);
-    // Show and save the filtered image.
-    cv::namedWindow("filtered image", cv::WINDOW_NORMAL);
-    cv::imshow("filtered image", filtered_img);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/01-max_filter_k3.png", filtered_img);
-    // Show and save the image filtered with the median filter.
+    cv::medianBlur(filtered_img_3, median_img, 3);
     cv::namedWindow("median image", cv::WINDOW_NORMAL);
     cv::imshow("median image", median_img);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/02-median_filter.png", median_img);
-    // Show and save the intensity enhanced.
+    cv::imwrite("./results/image1/02-median_filter.jpg", median_img, comp);
+    // Enhance intensity values.
+    cv::Mat enhanced_img = Filters::intensity_enhancement(median_img, 50);
+    cv::Mat log_enhanced_img = Tools::log_transform(median_img);
+    cv::imwrite("./results/image1/03-enhanced50.jpg", enhanced_img, comp);
+    cv::imwrite("./results/image1/03-log_transormed.jpg", log_enhanced_img,
+                comp);
     cv::namedWindow("enhanced image", cv::WINDOW_NORMAL);
-    cv::imshow("enhanced image", enhanced_img);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/03-log_transormed.png", enhanced_img);
-    // Show and save the histogram of the original image.
-    cv::imshow("original histogram", hist_img);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/00-original_histrogram.png", hist_img);
-    // Show and save the histogram of the filtered image.
-    cv::imshow("filtered histogram", hist_filtered);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/01-filtered_histrogram_k3.png", hist_filtered);
-    // Show and save the histogram of the median filtered image.
-    cv::imshow("median histogram", hist_median);
-    cv::waitKey(0);
-    cv::imwrite("./results/image1/02-median_histrogram.png", hist_median);
-    // Show and save the histogram of the log enhancd image.
-    cv::imshow("intensity enhanced histogram", hist_enhanced);
-    cv::imwrite("./results/image1/03-log_transormed_histrogram.png",
-                hist_enhanced);
+    cv::imshow("enhanced image", log_enhanced_img);
     cv::waitKey(0);
     return 0;
 }
